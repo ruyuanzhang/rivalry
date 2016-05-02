a7899e3f7971f4965e8859b1d9e20386d65d905f
Psycho Physics User <psphsuser@PsycPhysM.cmrr.umn.edu>
2016-05-02 12:31:15 -0500

    update img rotate settings

--- setupImgRotate.m
+++ setupImgRotate.m
@@ -1,11 +1,16 @@
+
 %compuate the rotation angle
 angleStep = 1;
-%found the correct rotation steps
-index = find(frameorder(1:framecnt)==0);
-angleStepCnt = framecnt - index(end)-1;
+if frameorder(framecnt)~=frameorder(framecnt-1)
+    angleStepCnt = 0;
+elseif frameorder(framecnt)==frameorder(framecnt-1)
+    angleStepCnt = angleStepCnt+1;
+end
+
 if angleStepCnt == 0
     rotDir = sign(rand-0.5); 
 end
+
 if rotate == 1
     rotangle = rotDir*angleStepCnt*angleStep;
 elseif rotate == 0
