19497d57ccd2393e572cb59f98a4d32e7f4eb07b
Psycho Physics User <psphsuser@PsycPhysM.cmrr.umn.edu>
2016-05-06 14:06:01 -0500

    change the rotation and contrast ratio

--- ptviewmovie3D.m
+++ ptviewmovie3D.m
@@ -1202,7 +1202,7 @@ for frame=1:frameskip:size(frameorder,2)+1
                 elseif stereoMode == 1||2 %present two different images to two eyes
                     Screen('SelectStereoDrawBuffer', win, 0);
                     %Screen('DrawTexture',win,texture,[],movierect,rotangle,filtermode,1,framecolor(frame0,:));
-                    Screen('DrawTexture',win,texture,[],movierect,rotangle,filtermode,1,[255 0 0]);
+                    Screen('DrawTexture',win,texture,[],movierect,rotangle,filtermode,1,[255 100 100]);
                     %Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
                     %Screen('DrawTexture',win,mask,[],movierect,[],[],[]); % we draw a 2D round mask
                     Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -1211,7 +1211,7 @@ for frame=1:frameskip:size(frameorder,2)+1
                     
                     Screen('SelectStereoDrawBuffer', win, 1);
                     %Screen('DrawTexture',win,texture2,[],movierect,-rotangle,filtermode,1,framecolor(frame0,:));
-                    Screen('DrawTexture',win,texture2,[],movierect,-rotangle,filtermode,1,[0 255 0]);
+                    Screen('DrawTexture',win,texture2,[],movierect,-rotangle,filtermode,1,[50 100 50]);
                     %Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
                     %Screen('DrawTexture',win,mask,[],movierect,[],[],[]); % we draw a 2D round mask
                     Screen('BlendFunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
