00e692359340976342a565052050d5c1c7d29ca7
7Tuser <7TPS-MacPro.cmrr.umn.edu>
2016-10-14 16:40:37 -0500

    test luca

--- constcalibration.m
+++ constcalibration.m
@@ -41,7 +41,7 @@ offset = [0 0];  % [] means no translation of the stimuli
 movieflip = [0 0];  % [0 0] means no flips.  [1 0] is necessary for flexi mirror to show up right-side up
 
 
-rblumconst=[127 1 127 1 1]; %initial contrast for left and right image
+rblumconst=[24 1 127 1 1]; %initial contrast for left and right image
 
 frameduration = 12;  % number of monitor frames for one unit.  60/5 = 12,120/5=24
 %ptonparams = {[1920 1080 120 24],[],0,skipsync,stereoMode};  % manually
@@ -97,7 +97,7 @@ plot(catconst(:,2),'b-o','lineWidth',2);ylim([0 1]);hold on;
 legend({'F','H','C'});
 
 c=fix(clock);
-filename=sprintf('%d%02d%02d%02d%02d%02d_test',c(1),c(2),c(3),c(4),c(5),c(6));
+filename=sprintf('%d%02d%02d%02d%02d%02d_sub%s_consttest',c(1),c(2),c(3),c(4),c(5),c(6),subj);
 save(filename);
 
 %save(['run' num2str(runnum)],'timeframes','timekeys');
